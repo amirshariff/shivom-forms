5f381968b88f78e02575b8d94269342203a3318a
amir shariff <amirshariff@msn.com>
2023-05-25 13:24:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -20,6 +20,6 @@ app.get("/", (req, res)=>{
 
 app.use(generatorRoute);
 
-app.listen(3000, ()=>{
-    console.log("listen on port 3000");
+app.listen(80, ()=>{
+    console.log("listen on port 80");
 });
